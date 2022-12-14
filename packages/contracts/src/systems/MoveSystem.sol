4f564e19f0844646a09eda2e607b0172be7c8a0d
Kevin Ingersoll <kingersoll@gmail.com>
2022-12-13 18:02:12 -0800

    Step 1: first ECS component and system

---
+++ packages/contracts/src/systems/MoveSystem.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
+import { System, IWorld } from "solecs/System.sol";
+import { getAddressById, addressToEntity } from "solecs/utils.sol";
+import { PositionComponent, ID as PositionComponentID, Coord } from "components/PositionComponent.sol";
+
+uint256 constant ID = uint256(keccak256("system.Move"));
+
+contract MoveSystem is System {
+  constructor(IWorld _world, address _components) System(_world, _components) {}
+
+  function execute(bytes memory args) public returns (bytes memory) {
+    return executeTyped(abi.decode(args, (Coord)));
+  }
+
+  function executeTyped(Coord memory coord) public returns (bytes memory) {
+    uint256 entityId = addressToEntity(msg.sender);
+
+    PositionComponent position = PositionComponent(getAddressById(components, PositionComponentID));
+    position.set(entityId, coord);
+  }
+}
