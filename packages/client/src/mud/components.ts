f1fe7210a1600e9e04abc6da6d98ad455742da12
Kevin Ingersoll <kingersoll@gmail.com>
2022-12-19 20:07:47 -0800

    Step 8: add terrain

--- packages/client/src/mud/components.ts
+++ packages/client/src/mud/components.ts
@@ -11,6 +11,7 @@ export const components = {
     {
       width: Type.Number,
       height: Type.Number,
+      terrain: Type.String,
     },
     {
       id: "MapConfig",
--- packages/contracts/src/systems/InitSystem.sol
+++ packages/contracts/src/systems/InitSystem.sol
@@ -3,6 +3,7 @@ pragma solidity >=0.8.0;
 import { System, IWorld } from "solecs/System.sol";
 import { getAddressById } from "solecs/utils.sol";
 import { MapConfigComponent, ID as MapConfigComponentID, MapConfig } from "components/MapConfigComponent.sol";
+import { TerrainType } from "../TerrainType.sol";
 
 uint256 constant ID = uint256(keccak256("system.Init"));
 
@@ -13,6 +14,47 @@ contract InitSystem is System {
     MapConfigComponent mapConfig = MapConfigComponent(getAddressById(components, MapConfigComponentID));
     if (mapConfig.isSet()) return new bytes(0);
 
-    mapConfig.set(MapConfig({ width: 10, height: 10 }));
+    // Alias these to make em easier to draw a tile map
+    TerrainType O = TerrainType.None;
+    TerrainType T = TerrainType.TallGrass;
+    TerrainType B = TerrainType.Boulder;
+
+    TerrainType[20][20] memory map = [
+      [O, O, O, O, O, O, T, O, O, O, O, O, O, O, O, O, O, O, O, O],
+      [O, O, T, O, O, O, O, O, T, O, O, O, O, B, O, O, O, O, O, O],
+      [O, T, T, T, T, O, O, O, O, O, O, O, O, O, O, T, T, O, O, O],
+      [O, O, T, T, T, T, O, O, O, O, B, O, O, O, O, O, T, O, O, O],
+      [O, O, O, O, T, T, O, O, O, O, O, O, O, O, O, O, O, T, O, O],
+      [O, O, O, B, B, O, O, O, O, O, O, O, O, O, O, O, O, O, O, O],
+      [O, T, O, O, O, B, B, O, O, O, O, T, O, O, O, O, O, B, O, O],
+      [O, O, T, T, O, O, O, O, O, T, O, B, O, O, T, O, B, O, O, O],
+      [O, O, T, O, O, O, O, T, T, T, O, B, B, O, O, O, O, O, O, O],
+      [O, O, O, O, O, O, O, T, T, T, O, B, T, O, T, T, O, O, O, O],
+      [O, B, O, O, O, B, O, O, T, T, O, B, O, O, T, T, O, O, O, O],
+      [O, O, B, O, O, O, T, O, T, T, O, O, B, T, T, T, O, O, O, O],
+      [O, O, B, B, O, O, O, O, T, O, O, O, B, O, T, O, O, O, O, O],
+      [O, O, O, B, B, O, O, O, O, O, O, O, O, B, O, T, O, O, O, O],
+      [O, O, O, O, B, O, O, O, O, O, O, O, O, O, O, O, O, O, O, O],
+      [O, O, O, O, O, O, O, O, O, O, B, B, O, O, T, O, O, O, O, O],
+      [O, O, O, O, T, O, O, O, T, B, O, O, O, T, T, O, B, O, O, O],
+      [O, O, O, T, O, T, T, T, O, O, O, O, O, T, O, O, O, O, O, O],
+      [O, O, O, T, T, T, T, O, O, O, O, T, O, O, O, T, O, O, O, O],
+      [O, O, O, O, O, T, O, O, O, O, O, O, O, O, O, O, O, O, O, O]
+    ];
+
+    uint32 height = uint32(map.length);
+    uint32 width = uint32(map[0].length);
+    bytes memory terrain = new bytes(width * height);
+
+    for (uint32 y = 0; y < height; y++) {
+      for (uint32 x = 0; x < width; x++) {
+        TerrainType terrainType = map[y][x];
+        if (terrainType == TerrainType.None) continue;
+
+        terrain[(y * width) + x] = bytes1(uint8(terrainType));
+      }
+    }
+
+    mapConfig.set(MapConfig({ width: width, height: height, terrain: string(terrain) }));
   }
 }
