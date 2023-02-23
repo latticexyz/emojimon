7147911d352a405cfe691b5e4b7e44e46c9cff75
Kevin Ingersoll <kingersoll@gmail.com>
2023-02-23 16:35:33 -0500

    Step 6: loading screen

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,9 +1,29 @@
+import { SyncState } from "@latticexyz/network";
+import { useComponentValue } from "@latticexyz/react";
+import { useMUD } from "./MUDContext";
 import { GameBoard } from "./GameBoard";
 
 export const App = () => {
+  const {
+    components: { LoadingState },
+    singletonEntity,
+  } = useMUD();
+
+  const loadingState = useComponentValue(LoadingState, singletonEntity, {
+    state: SyncState.CONNECTING,
+    msg: "Connecting",
+    percentage: 0,
+  });
+
   return (
     <div className="w-screen h-screen flex items-center justify-center">
-      <GameBoard />
+      {loadingState.state !== SyncState.LIVE ? (
+        <div>
+          {loadingState.msg} ({Math.floor(loadingState.percentage)}%)
+        </div>
+      ) : (
+        <GameBoard />
+      )}
     </div>
   );
 };
