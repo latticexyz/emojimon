fdc3dc7b733c3113aaaf666b0fbe5fae2e37fe3c
Kevin Ingersoll <kingersoll@gmail.com>
2023-02-27 10:55:34 -0500

    Step 10.2: add encounterable component to players

--- packages/contracts/src/systems/JoinGameSystem.sol
+++ packages/contracts/src/systems/JoinGameSystem.sol
@@ -3,6 +3,7 @@ pragma solidity >=0.8.0;
 import { addressToEntity } from "solecs/utils.sol";
 import { System, IWorld } from "solecs/System.sol";
 import { getAddressById } from "solecs/utils.sol";
+import { EncounterableComponent, ID as EncounterableComponentID } from "components/EncounterableComponent.sol";
 import { PlayerComponent, ID as PlayerComponentID } from "components/PlayerComponent.sol";
 import { PositionComponent, ID as PositionComponentID, Coord } from "components/PositionComponent.sol";
 import { MovableComponent, ID as MovableComponentID } from "components/MovableComponent.sol";
@@ -35,5 +36,6 @@ contract JoinGameSystem is System {
     player.set(entityId);
     PositionComponent(getAddressById(components, PositionComponentID)).set(entityId, coord);
     MovableComponent(getAddressById(components, MovableComponentID)).set(entityId);
+    EncounterableComponent(getAddressById(components, EncounterableComponentID)).set(entityId);
   }
 }
