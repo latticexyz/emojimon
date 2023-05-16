948490c3a3954d8849f9c8c8102f67fa6d1450ba
Kevin Ingersoll <kingersoll@gmail.com>
2023-05-16 15:43:11 -0700

    don't include keyboard movement yet

--- packages/client/src/GameBoard.tsx
+++ packages/client/src/GameBoard.tsx
@@ -1,8 +1,5 @@
 import { GameMap } from "./GameMap";
-import { useKeyboardMovement } from "./useKeyboardMovement";
 
 export const GameBoard = () => {
-  useKeyboardMovement();
-
   return <GameMap width={20} height={20} />;
 };
